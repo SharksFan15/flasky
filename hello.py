1951f3763853d8406c797c342d5f0c1872576eea
Nick Stanley <nickstanley1971@gmail.com>
2021-08-16 17:29:23 +0100

    Chapter 4. Add redirect

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
